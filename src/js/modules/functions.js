892bc0f97403d74a151e86f7d64fdd993d520c34
neverest1827 <103934692+neverest1827@users.noreply.github.com>
2022-09-06 20:01:25 +0300

    Update functions.js

--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const TOKEN = "";
-const CHAT_ID = "-1001734485530";
+const CHAT_ID = "";
 const URI_IP = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
 const success = document.querySelector('.success')
 const checkbox = document.querySelector('.form__label-checkbox')
